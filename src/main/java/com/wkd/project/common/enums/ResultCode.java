1169060e82b53b47bd03d4986858edd2a7ae1cd1
wang_kd601 <15968320362@163.com>
2021-08-20 09:18:20 +0800

    修改异常断言抛出

--- src/main/java/com/wkd/project/common/enums/ResultCode.java
+++ src/main/java/com/wkd/project/common/enums/ResultCode.java
@@ -1,5 +1,6 @@
 package com.wkd.project.common.enums;
 
+import com.wkd.project.common.exception.BusinessExceptionAssert;
 import lombok.Getter;
 
 /**
@@ -10,12 +11,15 @@ import lombok.Getter;
  * @createTime 2021-07-29 11:49
  */
 @Getter
-public enum ResultCode implements StatusCode{
+public enum ResultCode implements StatusCode, BusinessExceptionAssert {
+    /**
+     * 状态码枚举
+     */
     SUCCESS(1000, "请求成功"),
     FAILED(1001, "请求失败"),
     VALIDATE_ERROR(1002, "参数校验失败"),
     RESPONSE_PACK_ERROR(1003, "response返回包装失败"),
-    SERVER_ERROR(1004,"系统异常"),
+    SERVER_ERROR(1004, "系统异常"),
 
     /* 参数错误：1000～1999 */
     /*PARAM_NOT_VALID(1004, "参数无效"),
@@ -34,8 +38,6 @@ public enum ResultCode implements StatusCode{
     USER_ACCOUNT_ALREADY_EXIST(2008, "账号已存在"),
     USER_ACCOUNT_USE_BY_OTHERS(2009, "账号下线"),
     USER_NOT_NAME(2010, "用户名为空");
-
-
 
 
     private int code;
